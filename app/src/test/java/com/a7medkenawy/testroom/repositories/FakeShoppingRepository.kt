c751452ed099e6df4302f6d51af2c9d979b5cbb6
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-08-18 17:56:34 +0200

    add fake repo to test

--- app/src/test/java/com/a7medkenawy/testroom/repositories/FakeShoppingRepository.kt
+++ app/src/test/java/com/a7medkenawy/testroom/repositories/FakeShoppingRepository.kt
@@ -1,3 +1,59 @@
 package com.a7medkenawy.testroom.repositories
 
-class FakeShoppingRepository {}
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.a7medkenawy.testroom.data.local.ShoppingItem
+import com.a7medkenawy.testroom.data.remote.models.ImageResponse
+import com.a7medkenawy.testroom.data.repositories.ShoppingRepositories
+import com.a7medkenawy.testroom.presentaion.util.Recourse
+
+class FakeShoppingRepository : ShoppingRepositories {
+
+    val shoppingItems = mutableListOf<ShoppingItem>()
+
+    val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float? {
+        return shoppingItems.sumByDouble {
+            it.price.toDouble()
+        }.toFloat()
+    }
+
+    override fun getImage(searchQuery: String): Recourse<ImageResponse> {
+        return if (shouldReturnNetworkError) {
+            Recourse.OnSuccess(ImageResponse(listOf(), 0, 0))
+        } else {
+            Recourse.OnError("Error")
+        }
+    }
+
+    override fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun getAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun getAllShoppingItemsPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+}
